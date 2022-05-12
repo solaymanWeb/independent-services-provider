e5ae0a4f1404eaac352d8fb50db410694924d5ea
solayman1e <solaymanweb27@gmail.comm>
2022-05-12 13:04:22 +0600

    not found page compled

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import notfound from '../images/notfound.jpg'
 
 const NotFound = () => {
     return (
         <div>
-            <p>Not Found</p>
+            <img src={notfound} alt="" />
         </div>
     );
 };
